fbb5dab686f9fe9053832c78666e6c68d44d4a71
Mike Kao <skybow@gmail.com>
2020-04-25 16:31:33 +0800

    ver3

--- prod.py
+++ prod.py
@@ -1,7 +1,7 @@
 _1st_dimension = []
 while True:
 	_name = input('請輸入名稱: ')
-	if _name == 'q':
+	if _name == 'q':	# 當輸入 q 表示離開
 		break
 	_price = input('請輸入價格: ')
 # 寫法1 , 理解用
@@ -16,9 +16,12 @@ while True:
 # 7-10 行簡寫
 	_1st_dimension.append([_name, _price])
 
-print(_1st_dimension) # 測試 A1
+print(_1st_dimension)
 
 ## 以下說明
-# _1st_dimension[0][0] # 第1個[0] = 大清單第1個欄位
-# _1st_dimension[0][0] # 第2個[0] = 大清單第1個欄位裡的第1個欄位
-# print(_1st_dimension[0][0]) # 測試 B
+# _1st_dimension[0][0]	# 第1個[0] = 大清單第1個欄位
+# _1st_dimension[0][0]	# 第2個[0] = 大清單第1個欄位裡的第1個欄位
+
+for _2nd_dimension in _1st_dimension:
+	# print(_2nd_dimension)			# 測試A , 利用迴圈印出第二階所有內容
+	print(_2nd_dimension[0])		# 測試B , 利用迴圈印出第二階指定內容
