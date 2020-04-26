cfc9bd242760fc19386e3156b6cab0baf9fe28e1
Mike Kao <skybow@gmail.com>
2020-04-27 01:59:14 +0800

    ver5 add header

--- prod.py
+++ prod.py
@@ -30,6 +30,6 @@ with open('prod.csv', 'w', encoding='utf-8') as f:	# excel 直接開會亂碼 , 
 # with open('prod.csv', 'w') as f:	# excel 直接開正常 , sublime 開 py 檔案亂碼
 	# 上面說明
 	# with = 自動把open的檔案close , open = 打開電腦檔案(如果有會覆蓋,沒有會產生) , w = write , f = 命名, 編碼 = utf-8
-	f.write('商品,名稱\n')	#增加一行
+	f.write('商品,名稱\n')	# 增加欄位名稱
 	for _2nd_dimension in _1st_dimension:
 		f.write(_2nd_dimension[0] + ',' + _2nd_dimension[1] + '\n')	# write 真正寫入 , 用逗號區隔 , \n = 換行
